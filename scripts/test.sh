0c55795270deb5584696abf6859400bf100b24b4
Jon Michael Aanes <jon.michael.aanes@secata.com>
2024-01-31 12:01:39 +0100

    Testing golden runs

---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+pytest -v --tb=short --device nanos --display
+pytest -v --tb=short --device nanosp --display
+pytest -v --tb=short --device nanox --display
+pytest -v --tb=short --device stax --display
