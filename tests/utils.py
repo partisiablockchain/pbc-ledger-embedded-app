e4df69c600daef7f7bfa50df4e0475478487ddf9
Jon Michael Aanes <jon.michael.aanes@secata.com>
2024-02-09 13:45:44 +0100

    PAR-12190: Add chain id to UI

--- tests/utils.py
+++ tests/utils.py
@@ -4,4 +4,7 @@ ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
 
 KEY_PATH: str = "m/44'/3757'/0'/0/0"
 
-CHAIN_ID = b'Partisia Blockchain Testnet'
+CHAIN_IDS = [
+    b'Partisia Blockchain Testnet',
+    b'Partisia Blockchain',
+]
