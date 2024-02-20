b9486f7a4ec9dda4d32579524a46b33d19c9235e
Jon Michael Aanes <jon.michael.aanes@secata.com>
2024-02-20 14:32:09 +0100

    Removed debugging print

--- tests/generate_fuzzing_corpus.py
+++ tests/generate_fuzzing_corpus.py
@@ -9,7 +9,5 @@ import transaction_examples
 CORPUS_PATH = '../fuzzing/corpus/valid-examples'
 
 for transaction_name, transaction in transaction_examples.VALID_TRANSACTIONS:
-    print(transaction_name, transaction)
-
     with open('{}/{}'.format(CORPUS_PATH, transaction_name), 'wb') as f:
         f.write(transaction.serialize())
