1ecb67118b275c197eec0673acdaafe57775d3f6
Jon Michael Aanes <jon.michael.aanes@secata.com>
2024-02-20 13:31:03 +0100

    Removed slow printfs that significantly slowed down the fuzzing process.

--- fuzzing/fuzz_tx_parser.c
+++ fuzzing/fuzz_tx_parser.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 #include "transaction/deserialize.h"
 #include "transaction/types.h"
 #include "format.h"
 
+static bool check_status_invariants(parser_status_e status) {
+  // Zero is reserved.
+  if (status == 0) {
+    return false;
+  }
+
+  // Status must be known
+  return status == PARSING_DONE || status == PARSING_CONTINUE || PARSING_FAILED_MPC_MEMO <= status;
+}
+
 int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     buffer_t buf = {.ptr = data, .size = size, .offset = 0};
+
     transaction_t tx;
-    parser_status_e status;
-
-    char nonce[21] = {0};
-    char contract[ADDRESS_LEN * 2 + 1] = {0};
-    char gas_cost[21] = {0};
-    char valid_to_time[21] = {0};
-
     memset(&tx, 0, sizeof(tx));
 
     transaction_parsing_state_t state;
     transaction_parser_init(&state);
-    status = transaction_parser_update(&state, &buf, &tx);
+    parser_status_e status = transaction_parser_update(&state, &buf, &tx);
 
-    if (status == PARSING_DONE || status == PARSING_CONTINUE) {
-        format_u64(nonce, sizeof(nonce), tx.basic.nonce);
-        printf("nonce: %s\n", nonce);
-
-        format_hex(tx.basic.contract_address.raw_bytes, ADDRESS_LEN, contract, sizeof(contract));
-        printf("contract: %s\n", contract);
-
-        format_u64(gas_cost, sizeof(gas_cost), tx.basic.gas_cost);
-        printf("gas_cost: %s\n", gas_cost);
-
-        format_u64(valid_to_time, sizeof(valid_to_time), tx.basic.gas_cost);
-        printf("valid_to_time: %s\n", valid_to_time);
+    // Check that status cannot lie outside the expected set.
+    if (!check_status_invariants(status)) {
+      exit(1);
     }
 
     return 0;
